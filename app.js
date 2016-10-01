119064d1f5a75a4a946a4f0ea4ab92f3c95c74f7
nelfarna <elfarnadia@gmail.com>
2016-10-01 18:45:58 -0500

    updated routes and packages

--- app.js
+++ app.js
@@ -17,29 +17,36 @@
  *
  */
 
-var express    = require('express');
-var routes     = require('./routes'),
-	getstarted = require('./routes/getstarted'),
-	api 	   = require('./routes/api'),
-	photos     = require('./routes/photos');
+const 	express = require('express'),
+		cookieParser = require('cookie-parser'),
+		bodyParser = require('body-parser'),
+		session = require('express-session'),
+		logger = require('morgan'),
+		methodOverride = require('method-override'),
+		_ = require('underscore'),
+		MongoStore = require('connect-mongo')(session);
+
+const 	routes = require('./routes'),
+		getstarted = require('./routes/getstarted'),
+		api = require('./routes/api'),
+		photos = require('./routes/photos');
  
+const 	http  = require('http'),
+		path  = require('path'),
+		cons  = require('consolidate');
 
-var http  = require('http');
-var path  = require('path');
-var cons  = require('consolidate');
-
-var PORT = process.env.PORT || 5000;
-var HOSTNAME = process.env.HOST || ('localhost:' + PORT);
+const 	PORT = process.env.PORT || 5000,
+		HOSTNAME = process.env.HOST || ('localhost:' + PORT);
 
 // Instagram information (stored in Heroku config or .env for localhost)
-var ig_REDIRECT_route = process.env.INSTAGRAM_REDIRECT_END;
+const ig_REDIRECT_route = process.env.INSTAGRAM_REDIRECT_END;
 
 // Dropbox information (stored in Heroku config or .env for localhost)
-var db_REDIRECT_route = process.env.DROPBOX_REDIRECT_END;
+const db_REDIRECT_route = process.env.DROPBOX_REDIRECT_END;
 
-var app = express();
+const app = express();
 
-var clients = {};
+const clients = {};
 
 // all environments
 app.set('port', PORT)
@@ -47,22 +54,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 
+app.use(logger('dev'));
+app.use(methodOverride());
 
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.methodOverride());
+app.use(cookieParser());
+app.use(session({
+	secret: '123456SECRET',
+	store: new MongoStore({
+		url: process.env.MONGODB_URI
+	}),
+	resave: true,
+    saveUninitialized: true
+}));
 
-app.use(express.cookieParser());
-app.use(express.session({secret: '123456SECRET'}));
-app.use(express.bodyParser());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 // routes that require the session test
-
-var authRoutes = ['/photos', '/subscribe', '/zip', '/dbox', '/getstarted'];
-
+const authRoutes = ['/photos', '/subscribe', '/zip', '/dbox', '/getstarted'];
 
 // apply before routes to test session authentication
-function beforeFilter (req, res, next) {
+const beforeFilter = (req, res, next) => {
 	var exists = false;
     authRoutes.map(function (x) {
     	if (req.path.indexOf(x) !== -1) {
@@ -70,9 +85,11 @@ function beforeFilter (req, res, next) {
     	}
     });
 
-	if (!req.session.userId && exists) {
+	if ((!req.session.userId || _.isEmpty(clients)) && exists) {
         res.redirect('/igAuth'); // start authorization process
-    } else {
+    } 
+    else 
+    {
     	if(req.path.indexOf('drop') !== -1  && !clients[req.session.userId]['dbox']) { // if dropbox not authenticated
 			var data = JSON.stringify('/dbAuth')
 			res.header('Content-Length', data.length);
@@ -84,30 +101,15 @@ function beforeFilter (req, res, next) {
 
 app.use(beforeFilter);
 
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-var server = http.createServer(app).listen(PORT, function() {
+const server = http.createServer(app).listen(PORT, function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
 // development only
 if ('development' == app.get('env')) {
-   app.use(express.errorHandler());
-}
-
-function responseHandler (response) {
-	this.response = response;
-	var self = this;
-	this.doAction = function (func, param) {
-		switch (func) {
-			case 'redirect': this.response.redirect(param); break;
-			case 'send': this.response.send(param); break;
-			case 'download': this.response.download(param); break;
-			case 'contentType': this.response.contentType(param); break;
-			default: break;
-		}
-	};
+   app.use(errorHandler);
 }
 
 app.get('/', routes.index);
@@ -117,11 +119,11 @@ app.get('/getstarted', function(req, res) {
 });
 
 app.get('/igAuth', function(req, res) {
-	api.instagramAuthorize(new responseHandler(res));
+	api.instagramAuthorize(res);
 });
 
 app.get('/dbAuth', function(req, res) {
-	api.dropboxAuthorize(new responseHandler(res), req.session.userId);
+	api.dropboxAuthorize(res, req.session.userId);
 });
 
 // Handles any get requests from Dropbox
@@ -136,6 +138,7 @@ app.get(ig_REDIRECT_route, function(req, res, next) {
 
 // for Instagram subscription post notifications for new user posts
 app.post(ig_REDIRECT_route, function(req, res, next) {
+	console.log('INCOMING POST NOTIFICATION FROM INSTAGRAM');
 	api.instagramPost(req, res, clients, next);
 });
 
@@ -144,7 +147,7 @@ app.get('/subscribe', function(req, res, next) {
 });
 
 app.get('/photos', function(req, res) {
-	photos.main(req, res);
+	photos.main(req, res, clients[req.session.userId]);
 });
 
 // GET requests from client that don't require change in data on server-side
@@ -160,6 +163,3 @@ app.post('/photos/:type', function(req, res) {
 app.post('/zip', function(req, res, next) {
 	photos.startZip(clients[req.session.userId], req, res);
 });
-
-
-
